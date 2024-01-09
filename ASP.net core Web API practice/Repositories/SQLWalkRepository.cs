04fa68de09ddb09ce9837ae94fba870bc4b874e5
Tshindimpho <mudzu13@gmail.com>
2024-01-09 13:29:43 +0200

    Updated

--- ASP.net core Web API practice/Repositories/SQLWalkRepository.cs
+++ ASP.net core Web API practice/Repositories/SQLWalkRepository.cs
@@ -19,10 +19,59 @@ namespace Walk_and_Trails_of_SA_API.Repositories
             return walk;
         }
 
+        public async Task<Walk?> DeleteAsync(Guid id)
+        {
+           var existingWalk=  await databaseContext.Walks.FirstOrDefaultAsync(x => x.Id == id);
+
+            //CHECK
+
+            if (existingWalk == null)
+            {
+                return null;
+            }
+
+            databaseContext.Walks.Remove(existingWalk);
+            await databaseContext.SaveChangesAsync();
+            return existingWalk;
+        }
+
         public async Task<List<Walk>> GetALLAsync()
         {
-             return await databaseContext.Walks.Include("Difficulty").Include("Region").ToListAsync();
+             return await databaseContext.Walks
+                .Include("Difficulty")
+                .Include("Region")
+                .ToListAsync();
 
+        }
+
+        public async Task<Walk?> GetByIdAsync(Guid id)
+        {
+             return await databaseContext.Walks
+                .Include("Difficulty")
+                .Include("Region")
+                .FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<Walk?> UpdateByIdAsync(Guid id, Walk walk)
+        {
+            var existingWalk =await databaseContext.Walks.FirstOrDefaultAsync(x => x.Id == id);
+
+            //check
+
+            if (existingWalk != null)
+            {
+                return null;
+            }
+
+            existingWalk.Name = walk.Name;
+            existingWalk.Description = walk.Description;
+            existingWalk.LengthInkm= walk.LengthInkm;
+            existingWalk.WalkImaeUrl = walk.WalkImaeUrl;
+            existingWalk.DifficultyId = walk.DifficultyId;
+            existingWalk.RegionId = walk.RegionId;
+
+          await databaseContext.SaveChangesAsync();
+            return existingWalk;
         }
     }
 }
