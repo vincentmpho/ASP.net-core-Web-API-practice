9c101c208368689713fe28828fad747a8b573734
Tshindimpho <mudzu13@gmail.com>
2024-01-09 13:29:13 +0200

    implement updatedWalk DTO

---
+++ ASP.net core Web API practice/Models/DTO/UpdateWalkRequestDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Walk_and_Trails_of_SA_API.Models.DTO
+{
+    public class UpdateWalkRequestDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double LengthInkm { get; set; }
+        public string? WalkImaeUrl { get; set; }
+        public Guid DifficultyId { get; set; }
+        public Guid RegionId { get; set; }
+    }
+}
