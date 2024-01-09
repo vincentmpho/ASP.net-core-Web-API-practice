6c49c6172c2f0b760b1fa9a80da08aaa7256e59a
Tshindimpho <mudzu13@gmail.com>
2024-01-09 12:38:52 +0200

    Implement Difficulty DTO

---
+++ ASP.net core Web API practice/Models/DTO/DifficultyDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Walk_and_Trails_of_SA_API.Models.DTO
+{
+    public class DifficultyDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    
+    }
+}
