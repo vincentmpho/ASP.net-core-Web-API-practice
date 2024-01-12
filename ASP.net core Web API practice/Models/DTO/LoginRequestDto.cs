959e2c8dd51a53c572c516eeefc078e57f504674
Tshindimpho <mudzu13@gmail.com>
2024-01-12 12:37:59 +0200

    implement LoginRequestDto

---
+++ ASP.net core Web API practice/Models/DTO/LoginRequestDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Walk_and_Trails_of_SA_API.Models.DTO
+{
+    public class LoginRequestDto
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+    }
+}
