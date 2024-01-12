bb9f55ce19804d2be97641a36342e6a46aaade66
Tshindimpho <mudzu13@gmail.com>
2024-01-12 13:17:10 +0200

    Implement the Tokn interface and Repo

---
+++ ASP.net core Web API practice/Repositories/ITokenRepository.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Walk_and_Trails_of_SA_API.Repositories
+{
+    public interface ITokenRepository
+    {
+
+        string CreateJWTToken(IdentityUser user, List<string>roles);
+    }
+}
