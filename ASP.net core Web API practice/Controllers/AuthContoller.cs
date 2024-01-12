03a8cd4d19c121b0d025856a5c5e7b9c8313dcdd
Tshindimpho <mudzu13@gmail.com>
2024-01-12 14:18:43 +0200

    Updated endpoint

--- ASP.net core Web API practice/Controllers/AuthContoller.cs
+++ ASP.net core Web API practice/Controllers/AuthContoller.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Walk_and_Trails_of_SA_API.Models.DTO;
+using Walk_and_Trails_of_SA_API.Repositories;
 
 namespace Walk_and_Trails_of_SA_API.Controllers
 {
@@ -10,10 +11,12 @@ namespace Walk_and_Trails_of_SA_API.Controllers
     public class AuthContoller : ControllerBase
     {
         private readonly UserManager<IdentityUser> userManager;
+        public ITokenRepository TokenRepository { get; }
 
-        public AuthContoller(UserManager<IdentityUser> userManager)
+        public AuthContoller(UserManager<IdentityUser> userManager, ITokenRepository tokenRepository)
         {
             this.userManager = userManager;
+            TokenRepository = tokenRepository;
         }
 
         //Post: api/auth/Register
@@ -43,9 +46,48 @@ namespace Walk_and_Trails_of_SA_API.Controllers
                     return StatusCode(StatusCodes.Status200OK, "User was registered! Please login");
                 }
             }
-
             return StatusCode(StatusCodes.Status400BadRequest, "Something went wrong");
-
         }
+        //POST: api/api/Auth/Login
+
+        [HttpPost]
+        [Route("Login")]
+
+        public async Task<IActionResult> Login([FromBody] LoginRequestDto loginRequestDto)
+        {
+            var user = await userManager.FindByEmailAsync(loginRequestDto.UserName);
+
+            //check
+
+            if (user == null)
+            {
+                var checkPasswordResult = await userManager.CheckPasswordAsync(user, loginRequestDto.Password);
+
+                if (checkPasswordResult)
+                {
+                    //Get Roles For this roles
+
+                    var roles = await userManager.GetRolesAsync(user);
+
+                    if (roles != null)
+                    {
+                        //create token
+                         var jwtToken=TokenRepository.CreateJWTToken(user, roles.ToList());
+
+                        var response = new LoginResponseDto
+                        {
+                            JwtToken = jwtToken
+                        };
+                        return StatusCode(StatusCodes.Status200OK, jwtToken);
+                    }
+
+
+
+                    return StatusCode(StatusCodes.Status200OK);
+                }
+            }
+            return StatusCode(StatusCodes.Status400BadRequest, "username or Password incorrect");
+        }
+
     }
 }
