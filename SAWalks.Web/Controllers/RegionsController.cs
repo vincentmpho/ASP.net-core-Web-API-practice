c3558a6b3030a006420c43893a76a8b3a8859284
Tshindimpho <mudzu13@gmail.com>
2024-01-20 23:46:31 +0200

    Comsume Web API

---
+++ SAWalks.Web/Controllers/RegionsController.cs
@@ -0,0 +1,142 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SAWalks.Web.Models;
+using SAWalks.Web.Models.DTO;
+using Serilog;
+using System.Net.Http;
+using System.Reflection;
+using System.Text;
+using System.Text.Json;
+
+namespace SAWalks.Web.Controllers
+{
+    public class RegionsController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public RegionsController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
+        public async Task <IActionResult> Index()
+        {
+            List<RegionDto> Response = new List<RegionDto>();
+
+            try
+            {
+                //Get All Regions from API
+                var client = _httpClientFactory.CreateClient();
+
+                var httpResponseMessage = await client.GetAsync("https://localhost:7237/api/Regions");
+
+                httpResponseMessage.EnsureSuccessStatusCode();
+
+                Response.AddRange(await httpResponseMessage.Content.ReadFromJsonAsync<IEnumerable<RegionDto>>());
+            }
+            catch (Exception ex)
+            {
+                //log the exception
+                Log.Error(ex, "An error occurred while getting regions from the API");
+            }
+
+
+            return View(Response);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddRegionViewModel model)
+        {
+            var client = _httpClientFactory.CreateClient();
+
+            var httpRequestMessage = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new Uri("https://localhost:7237/api/Regions"),
+                Content = new StringContent(JsonSerializer.Serialize(model), Encoding.UTF8, "application/json")
+            };
+
+           var httpResponseMessage = await client.SendAsync(httpRequestMessage);
+            httpResponseMessage.EnsureSuccessStatusCode();
+
+             var respose =await httpResponseMessage.Content.ReadFromJsonAsync<IEnumerable<RegionDto>>();
+
+            if (respose is not null)
+            {
+                return RedirectToAction("Index", "Regions");
+            }
+            return View();
+        }
+
+        [HttpGet]
+
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var client = _httpClientFactory.CreateClient();
+
+            var response =await client.GetFromJsonAsync<RegionDto>($"https://localhost:7237/api/Regions/{id.ToString()}");
+
+            if (response is not null)
+            {
+                return View(response);
+            }
+
+            return View(null);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(RegionDto request)
+        {
+            var client = _httpClientFactory.CreateClient();
+
+            var httpRequestMessage = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Put,
+                RequestUri = new Uri($"https://localhost:7237/api/Regions/{request.Id}"),
+                Content = new StringContent(JsonSerializer.Serialize(request), Encoding.UTF8, "application/json")
+            };
+
+            var httpResponseMessage = await client.SendAsync(httpRequestMessage);
+            httpResponseMessage.EnsureSuccessStatusCode();
+
+            var respose = await httpResponseMessage.Content.ReadFromJsonAsync<IEnumerable<RegionDto>>();
+
+            if(respose is not null)
+            {
+                return RedirectToAction("Edit", "Regions");
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete (RegionDto request)
+        {
+            try
+            {
+                var client = _httpClientFactory.CreateClient();
+
+                var httpResponseMessage = await client.DeleteAsync($"https://localhost:7237/api/Regions/{request.Id}");
+
+                httpResponseMessage.EnsureSuccessStatusCode();
+
+                return RedirectToAction("Index", "Regions");
+            }
+            catch (Exception ex)
+            {
+
+                // Log the exception message
+                Log.Error(ex, "An error occurred while deleting the region");
+            }
+
+           return View("Edit");
+        }
+
+    }
+   
+}
